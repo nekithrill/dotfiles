6fdc3f71c4c1009020dda31c919882ffceed3907
nekithrill <nekithrill@gmail.com>
2026-06-10 14:39:16 +0300

    cleanup: delete unnecessary or old files

--- dotfiles/config/hypr/hyprland/env-nvidia.lua
+++
@@ -1,7 +0,0 @@
--- Uncomment the source line in hyprland.lua if you have NVIDIA GPU
-hl.env({
-    GBM_BACKEND = "nvidia-drm",
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia",
-    LIBVA_DRIVER_NAME = "nvidia",
-    WLR_NO_HARDWARE_CURSORS = "1",
-})
